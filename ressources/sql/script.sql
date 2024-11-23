13076f803c41a7221b183c6e74a6c6337eb663dd
Sosthène NOUEBISSI <sosthenenouebissi@gmail.com>
2024-11-23 17:59:37 +0100

    Mise à jour du script SQL

--- ressources/sql/script.sql
+++ ressources/sql/script.sql
@@ -2,6 +2,7 @@ CREATE TABLE `USERS` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `email` varchar(255),
   `name` varchar(255),
+  `enabled` boolean,
   `password` varchar(255),
   `created_at` timestamp,
   `updated_at` timestamp
@@ -28,10 +29,31 @@ CREATE TABLE `MESSAGES` (
   `updated_at` timestamp
 );
 
-CREATE UNIQUE INDEX `USERS_index` ON `USERS` (`email`);
+CREATE TABLE `ROLES` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `authority` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_authority` (`authority`)
+);
+
+CREATE TABLE `USERS_AUTHORITIES` (
+  `app_user_id` integer NOT NULL,
+  `authorities_id` integer NOT NULL
+);
+
+
+
+CREATE UNIQUE INDEX `USERS_indexusersusers` ON `USERS` (`email`);
 
 ALTER TABLE `RENTALS` ADD FOREIGN KEY (`owner_id`) REFERENCES `USERS` (`id`);
 
 ALTER TABLE `MESSAGES` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
 
 ALTER TABLE `MESSAGES` ADD FOREIGN KEY (`rental_id`) REFERENCES `RENTALS` (`id`);
+
+ALTER TABLE `USERS_AUTHORITIES` ADD FOREIGN KEY (`app_user_id`) REFERENCES `USERS` (`id`);
+
+ALTER TABLE `USERS_AUTHORITIES` ADD FOREIGN KEY (`authorities_id`) REFERENCES `ROLES` (`id`);
+
+INSERT INTO ROLES (authority) VALUES ('ROLE_USER');
+
+INSERT INTO ROLES (authority) VALUES ('ROLE_ADMIN');
